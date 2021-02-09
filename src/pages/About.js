0743634d8ed82df372253bfa52b765c5d392d8cc
Nate M King <72567521+natemking@users.noreply.github.com>
2021-02-08 20:48:52 -0500

    Fixed comment error

--- src/pages/About.js
+++ src/pages/About.js
@@ -3,7 +3,7 @@ import About from '../components/About';
 import fadeChoice from '../utils/fadeChoice'
 
 const AboutPage = () => {
-      // Render resume w/ Fade in effect if the screen is wider than 500px.
+      // Render About component w/ Fade in effect if the screen is wider than 500px.
     return ( 
         <>
             { fadeChoice(About) }
--- src/pages/Resume.js
+++ src/pages/Resume.js
@@ -4,7 +4,7 @@ import fadeChoice from '../utils/fadeChoice'
 
 const Resume = () => {
     return (
-        // Render resume w/ Fade in effect if the screen is wider than 500px.
+        // Render ResumeContainer component w/ Fade in effect if the screen is wider than 500px.
         <>
             { fadeChoice(ResumeContainer) }
         </>
