b3db3304bd3ceaaa5b25bd1862b4b2ca10b0b989
Nate M King <72567521+natemking@users.noreply.github.com>
2021-04-07 19:52:28 -0400

    Added HTML for the gallery image

--- src/components/Gallery/index.js
+++ src/components/Gallery/index.js
@@ -6,60 +6,81 @@ import 'photoswipe/dist/photoswipe.css'
 import 'photoswipe/dist/default-skin/default-skin.css'
 
 import { Gallery, Item } from 'react-photoswipe-gallery'
-import { waitForDomChange } from '@testing-library/dom';
+  
 
 const MyGallery = () => {
-     
     const deck = work.map(work => (
-        <Item 
-            id={ work.id }
-            original={ work.gif.default }
-            thumbnail={ work.imgSmall.default } 
-            alt={work.alt} 
-            width='640' 
-            height='430'
-            title={ work.desc } 
+       
+        
+        <Item
             key={work.id}
+            // id={work.id}
+            // original={work.gif.default}
+            // thumbnail={work.imgSmall.default}
+            // alt={work.alt}
+            // width='640'
+            // height='430'
+            // title={work.name}
+            html={`
+                <section class='gallery__jumbo-container'>
+                    <section class='mt-3 gallery__jumbo'>
+    
+                        <section className='mt-5'>
+                            <img src='${work.gif.default}'></img>
+                        </section>
+    
+                        <section class='gallery__jumbo-text' >
+                            <h4 class='text-right'>${work.name}</h4>
+                            <hr />
+                            <h5>
+                                About.
+                            </h5>
+                            <p>
+                                ${work.desc}
+                            </p>
+                            <hr />
+                            <h5>
+                                Tools.
+                            </h5>
+                            <p>
+                                ${work.tools}
+                            </p>
+                            <hr />
+                            <p>
+                                <a href=${ work.url } target='_blank' rel='noreferrer'>
+                                    Deploy
+                                </a> 
+                            </p>
+                            <p>
+                                <a href=${work.gitHub} target='_blank' rel='noreferrer'>
+                                    Git Repo
+                                </a>
+                            </p>
+                        </section>
+                    </section>
+                </section>
+            `}
         >
             {({ ref, open }) => (
-                <img ref={ref} onClick={open} src={work.imgSmall.default} alt={work.alt} id={ work.idName } />
+                <img ref={ref} onClick={open} src={work.imgSmall.default} alt={work.alt} id={work.idName} />
             )}
-            
-        </Item>
-     ));
 
-   return ( 
-       <section className='container col-9-md mt-3 gallery'>
+        </Item>
+    ));
+
+    return (
+        <section className='container col-9-md mt-3 gallery'>
             <h3>
                 Work.
             </h3>
 
             <Gallery>
-                {/* <Item
-                    original="https://placekitten.com/1024/768?image=1"
-                    thumbnail="https://placekitten.com/80/60?image=1"
-                    width="1024"
-                    height="768"
-                >
-                    {({ ref, open }) => (
-                        <img ref={ref} onClick={open} src="https://placekitten.com/80/60?image=1" />
-                    )}
-                </Item>
-                <Item
-                    original="https://placekitten.com/1024/768?image=2"
-                    thumbnail="https://placekitten.com/80/60?image=2"
-                    width="1024"
-                    height="768"
-                >
-                    {({ ref, open }) => (
-                        <img ref={ref} onClick={open} src="https://placekitten.com/80/60?image=2" />
-                    )}
-                </Item> */}
                 <section className='gallery__deck-container'>
-                    { deck }
+                    {deck}
                 </section>
             </Gallery>
-         </section>
+
+        </section>
     );
 }
 
