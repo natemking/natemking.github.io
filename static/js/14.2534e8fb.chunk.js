7a51cf1e5a10c496a78911b6a8620b706dfd9abb
Nate M King <72567521+natemking@users.noreply.github.com>
2021-04-07 09:51:33 -0400

    Updates

--- static/js/14.2534e8fb.chunk.js
+++
@@ -1,2 +0,0 @@
-(this["webpackJsonpportfolio_2.0"]=this["webpackJsonpportfolio_2.0"]||[]).push([[14],{107:function(t,s,e){"use strict";e.r(s);e(0);var i=e.p+"static/media/nate-in-smoke.26744637.jpg",a=e(1);s.default=function(){return Object(a.jsx)("img",{className:"wrapper__img",src:i,alt:"nate in smoke"})}}}]);
-//# sourceMappingURL=14.2534e8fb.chunk.js.map
---
+++ static/js/14.87beeaf3.chunk.js
@@ -0,0 +1,2 @@
+(this["webpackJsonpportfolio_2.0"]=this["webpackJsonpportfolio_2.0"]||[]).push([[14],{111:function(t,s,e){"use strict";e.r(s);e(0);var i=e.p+"static/media/nate-in-smoke.26744637.jpg",a=e(1);s.default=function(){return Object(a.jsx)("img",{className:"wrapper__img",src:i,alt:"nate in smoke"})}}}]);
+//# sourceMappingURL=14.87beeaf3.chunk.js.map
